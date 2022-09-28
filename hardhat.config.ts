3cf221d05a0c448fd77987bf8e7f141623420972
Mauro Tommasi <99327537+devCryptoMT@users.noreply.github.com>
2022-09-28 07:40:44 +0100

    Poseidon-DAO/metaborg-stars/#2
    Update to Goerli

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,47 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-chai-matchers";
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+const { API_URL, PRIVATE_KEY } = process.env;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+module.exports = {
+    solidity: {
+        // compilers: [{ version: "0.4.18", settings: {} }],
+        // compilers: [{ version: "0.5.0", settings: {} }],
+        // compilers: [{ version: "0.7.6", settings: {} }],
+        compilers: [
+            {
+            version: "0.8.0"
+            },
+            {
+            version: "0.8.1"
+            }, 
+            {
+            version: "0.8.3", 
+            settings: {} 
+            }
+        ],
+    },
+    defaultNetwork: "rinkeby",
+    networks: {
+        hardhat: {},
+        rinkeby: {
+            url: API_URL,
+            accounts: [`0x${PRIVATE_KEY}`]
+        },
+        goerli: {
+          url: API_URL,
+          accounts: [`0x${PRIVATE_KEY}`]
+        },
+        mainnet: {
+            url: API_URL,
+            accounts: [`0x${PRIVATE_KEY}`]
+        }
+    },
+    etherscan: {
+        // Your API key for Etherscan
+        // Obtain one at https://etherscan.io/
+        apiKey: ETHERSCAN_API_KEY
+    }
 };
-
-export default config;
