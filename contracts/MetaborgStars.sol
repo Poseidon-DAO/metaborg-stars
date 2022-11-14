e268f7bce54ac1106abbe62992cd997630d8af90
Mauro Tommasi <99327537+devCryptoMT@users.noreply.github.com>
2022-11-14 19:11:59 +0400

    First working commit

--- contracts/MetaborgStars.sol
+++ contracts/MetaborgStars.sol
@@ -10,24 +10,30 @@ import '@openzeppelin/contracts-upgradeable/utils/StringsUpgradeable.sol';
 contract MetaborgStars is ERC721Upgradeable {
 
     using SafeMathUpgradeable for uint256;
+    using SafeMathUpgradeable for uint8;
     using StringsUpgradeable for uint256; 
 
     uint private randomizerIndex;
-    uint public pagesAvailable;
     address public owner;
     uint private ownerBalance;
-    uint8[] public availablePagesArray;
+    uint8[] private availablePagesArray; 
+    uint8[] private availableStarsArray; 
     uint public blockDelay;
     address public ERC1155Address;
     string public baseURI;
+    uint8 visibility;
 
     /*
         @dev: It will be a waterfall check based on the order specified by priority
     */
-    enum userGroup {
-        OPEN,           // priority 0
-        WHITELISTED,    // priority 1
-        OWNER           // priority 2
+
+    enum visibilityInfo {
+        OPEN,
+        WHITELISTED,
+        OWNER,
+        OWNER_OR_WHITELISTED,
+        OWNER_AND_WHITELISTED,
+        CLOSED
     }
 
     struct groupPriceStruct {
@@ -51,24 +57,25 @@ contract MetaborgStars is ERC721Upgradeable {
     event setContactPhysicalEditionEvent(uint tokenID, bytes32 email);
     event setBlockDelayEvent(uint oldDelay, uint newDelay);
     event setWhitelistEvent(address indexed to, bool isWhitelisted);
-
+    event revealURIEvent(string oldURI, string newURI);
     
     modifier onlyOwner {
         require(owner == msg.sender, "ONLY_OWNER_CAN_RUN_THIS_FUNCTION");
         _;
     }
 
-    function initialize(uint _elements, string memory _baseURI, address _ERC1155Address) initializer public {
+    function initialize(uint[] memory _availableIDs, uint8[] memory _stars, string memory _baseURI, address _ERC1155Address) initializer public {
         __ERC721_init("Metaborg Five Stars by Giovanni Motta", "Metaborg Five Stars"); 
         owner = msg.sender;
         ERC1155Address = _ERC1155Address;
-        require(_elements < uint(256), "IPFS_LIST_TOO_LONG"); // Due to uint8 and project requirements
-        for(uint index = uint(0); index < _elements; index++){
-            availablePagesArray.push(uint8(index));
+        require(_stars.length < uint(256), "IPFS_LIST_TOO_LONG"); // Due to uint8 and project requirements
+        for(uint index = uint(0); index < _stars.length; index++){
+            require(_stars[index] >= 0 && _stars[index] <= 5, "STAR_VALUE_NOT_VALID");
+            availableStarsArray.push(uint8(_stars[index]));
+            availablePagesArray.push(uint8(_availableIDs[index]));
         }
         baseURI = _baseURI;
-        pagesAvailable = _elements;
-        emit initializeDataEvent(_elements, keccak256((abi.encodePacked(_baseURI))), _ERC1155Address);
+        emit initializeDataEvent(_stars.length, keccak256((abi.encodePacked(_baseURI))), _ERC1155Address);
     }
 
     function setWhitelistedAddresses(address[] memory _addresses, bool _toWhitelist) public onlyOwner returns(bool){
@@ -85,27 +92,28 @@ contract MetaborgStars is ERC721Upgradeable {
         @usr: We can have 4 cases: 0 (open), 1 (whitelisted), 2 (owner), 3 (whitelisted + owner)
     */
     function getUserGroup(address _address) public view returns(uint){
-        uint result = uint(userGroup.OPEN);
+        uint result = uint(visibilityInfo.OPEN); // = 0
         uint METABORG_DIAMOND_ID = uint(1);
         uint METABORG_GOLD_ID = uint(2);
         uint METABORG_ORIGINAL_ID = uint(3);
         IERC1155Upgradeable IERC1155 = IERC1155Upgradeable(ERC1155Address);
         require(ERC1155Address != address(0), "ERC1155_NOT_SET");
         if(isWhitelisted[_address]) {
-            result = uint(userGroup.WHITELISTED);
+            result = result.add(uint(visibilityInfo.WHITELISTED)); // = 1
         }
         if(IERC1155.balanceOf(_address, METABORG_DIAMOND_ID) > 0 || IERC1155.balanceOf(_address, METABORG_GOLD_ID) > 0 || IERC1155.balanceOf(_address, METABORG_ORIGINAL_ID) > 0) {
-            result = uint(userGroup.OWNER);
+            result = result.add(uint(visibilityInfo.OWNER)); // 2
         }
+        // = 3 if both
         return result;
     }
     /*
-        @dev: Group ID is equals to 0 (open), 1 (whitelisted), 2 (owner)
+        @dev: Group ID is equals to 0 (open), 1 (whitelisted), 2 (owner), 3 (whitelisted+owner)
     */
     function setGroupMetaData(uint[] memory _prices, uint[] memory _packs, uint _groupID) public onlyOwner returns(bool){
         require(_prices.length == uint(3), "PRICE_ARRAY_LENGTH_DISMATCH");
         require(_packs.length == uint(3), "PACKS_ARRAY_LENGTH_DISMATCH");
-        require(_groupID <= uint(userGroup.OWNER), "GROUP_ID_NOT_VALID");
+        require(_groupID < uint(visibilityInfo.CLOSED), "GROUP_ID_NOT_VALID");
         groupPriceMetaData[_groupID].pack1 = _packs[0];
         groupPriceMetaData[_groupID].pack2 = _packs[1];
         groupPriceMetaData[_groupID].pack3 = _packs[2];
@@ -122,11 +130,24 @@ contract MetaborgStars is ERC721Upgradeable {
         return true;
     }
 
+    function setVisibility(uint8 _visibility) public onlyOwner returns(bool){
+        require(visibility != _visibility, "SWITCH_DISMATCH");
+        visibility = _visibility;
+        return true;
+    }
+
     // OPENSEA COMPATIBILITY OVERRIDE
     // BaseURI Example: "https://<your-gateway>.mypinata.cloud/ipfs/<CID-Folder>/"
 
     function tokenURI(uint256 _tokenId) public override view returns (string memory) {
         return string(abi.encodePacked(baseURI,_tokenId.toString(),".json"));
+    }
+
+    function revealURI(string memory _newBaseURI) public onlyOwner returns(bool){
+        string memory oldBaseURI = baseURI;
+        baseURI = _newBaseURI;
+        emit revealURIEvent(oldBaseURI, _newBaseURI);
+        return true;
     }
 
     // RANDOMIZER LOGIC
@@ -139,49 +160,111 @@ contract MetaborgStars is ERC721Upgradeable {
         return number;
     }
 
-    function getRandom(uint _externalMax) private returns(uint){
+    function getRandom(uint _externalMax) public returns(uint){ // _externalmax = numberOfElements
+        require(_externalMax > 0, "CANT_DIVIDE_BY_ZERO");
         uint randomNumber = bytesToUint(bytes32(keccak256(abi.encodePacked(msg.sender, block.timestamp, block.number, address(this), randomizerIndex++))));
         return randomNumber.mod(_externalMax);
     }
 
-    function shiftArray8(uint8[] memory _array, uint _indexToDelete) public pure returns(uint8[] memory){
-        for(uint index = uint(0); index < _array.length.sub(1); index++){
-            if(index >= _indexToDelete) {
-                _array[index] = _array[index.add(1)];            
+    function shiftArray8(uint8[] memory _array, uint8[] memory _indexesToDelete) public pure returns(uint8[] memory){ 
+        uint k;
+        bool f;
+        uint8[] memory r = new uint8[](_array.length.sub(_indexesToDelete.length));
+        for(uint i = uint(0); i < _array.length; i++){
+            for(uint j = uint(0); j < _indexesToDelete.length; j++){
+                f || i == _indexesToDelete[j] ? f = true : true; 
             }
+            !f ? r[k++] = _array[i]: uint(0);
+            f = false;
         }
-        return _array;
+        return r;
     }
-
+ 
     function buyMetaborgStars() public payable returns(uint8[] memory){
-        (uint pack1, uint pack2, uint pack3, uint price1, uint price2, uint price3) = getAddressMetadata(msg.sender);
+        uint checkUserGroup = getUserGroup(msg.sender);
+        // VISIBILITY CHECKING
+        if(visibility == uint8(visibilityInfo.OPEN)){ // OPEN TO EVERYONE
+            require(checkUserGroup <= uint(3), "RESERVED_FUNCTION");
+        }
+        if(visibility == uint8(visibilityInfo.OWNER)){ // OPEN TO OWNER
+            require(checkUserGroup == uint(1), "RESERVED_FUNCTION");
+        }
+        if(visibility == uint8(visibilityInfo.WHITELISTED)){ // OPEN TO WHITELISTED
+            require(checkUserGroup == uint(2), "RESERVED_FUNCTION");
+        }
+        if(visibility == uint8(visibilityInfo.OWNER_OR_WHITELISTED)){ // OPEN TO OWNER OR WHITELISTED
+            require(checkUserGroup == uint(1) || checkUserGroup == uint(2), "RESERVED_FUNCTION");
+        }
+        if(visibility == uint8(visibilityInfo.OWNER_AND_WHITELISTED)){ // OPEN TO OWNER AND WHITELISTED
+            require(checkUserGroup == uint(3), "RESERVED_FUNCTION");
+        }
+        if(visibility >= uint8(visibilityInfo.CLOSED)){ // CLOSED
+             revert();
+        }
+        // GET METADATA
+        (uint8 pack1, uint8 pack2, uint8 pack3, uint price1, uint price2, uint price3) = getAddressMetadata(msg.sender);
         require(pack1 > uint(0), "UNDETECTED_METADATA");
-        uint packsPagesNumber;
+        uint8 packsPagesNumber;
+        uint8[] memory tmpPagesAvailable = availablePagesArray;
+        uint8[] memory tmpStarsAvailable = availableStarsArray;
         if(price1 == msg.value) packsPagesNumber = pack1;
         if(price2 == msg.value) packsPagesNumber = pack2;
         if(price3 == msg.value) packsPagesNumber = pack3;
         uint8[] memory randomIDList = new uint8[](uint(packsPagesNumber)); 
         require(packsPagesNumber > 0, "NOT_VALID_MSG_VALUE");
-        require(pagesAvailable >= packsPagesNumber, "NOT_ENOUGH_PAGES_AVAILABLE");
+        require(tmpPagesAvailable.length >= packsPagesNumber, "NOT_ENOUGH_PAGES_AVAILABLE");
+        // BUYING SYSTEM
+        bool forceStar;
+        bool specialPage;
+        uint8 pageID;
+        uint stars;
         for(uint index = uint(0); index < packsPagesNumber; index++) {
-            randomIDList[index] = buySinglePageAndGetPageID();
+            if(index == packsPagesNumber.sub(1) && !forceStar && !specialPage && (packsPagesNumber == pack2 || packsPagesNumber == pack3)) forceStar = true;
+            (pageID, tmpPagesAvailable, tmpStarsAvailable, stars) = buySinglePageAndGetPageID(tmpPagesAvailable, tmpStarsAvailable, forceStar);
+            if(stars == uint(3) || stars == uint(4)) specialPage = true; 
+            randomIDList[index] = uint8(pageID);
         }
+        availablePagesArray = new uint8[](tmpPagesAvailable.length);
+        availableStarsArray = new uint8[](tmpStarsAvailable.length);
+        availablePagesArray = tmpPagesAvailable;
+        availableStarsArray = tmpStarsAvailable;
         ownerBalance = ownerBalance.add(msg.value);
         return randomIDList;
     }    
 
-    function buySinglePageAndGetPageID() private returns(uint8){
-        uint tmpPagesAvailable = pagesAvailable;
-        uint8[] memory availablePagesArrayTmp = availablePagesArray;
-        uint randomIndex = getRandom(tmpPagesAvailable);
-        uint pageID = availablePagesArrayTmp[randomIndex];
-        availablePagesArray = new uint8[](uint(tmpPagesAvailable));
-        availablePagesArray = shiftArray8(availablePagesArrayTmp, randomIndex);
-        availablePagesArray.pop();
-        pagesAvailable = tmpPagesAvailable.sub(1);
-        _safeMint(msg.sender, pageID);
-        expirationBlock[pageID] = (block.number).add(blockDelay);
-        return uint8(pageID);
+    function airdropManga(address[] memory _addresses, uint[] memory _IDs) public onlyOwner returns(bool){
+        require(_addresses.length == _IDs.length, "LENGHT_DISMATCH");
+        for(uint index = uint(0); index < _addresses.length; index++){
+            _safeMint(_addresses[index], _IDs[index]);
+        }
+        return true;
+    }
+
+    function getForcedStarArray(uint8[] memory _starsArray) private pure returns(uint8[] memory, uint){
+        uint resultIndex = 0;
+        for(uint index = uint(0); index < _starsArray.length; index++){
+            if(_starsArray[index] == uint(3) || _starsArray[index] == uint(4)) {
+                _starsArray[resultIndex] = uint8(index);
+                resultIndex++;
+            }
+        }
+        return (_starsArray, resultIndex); 
+    }
+
+    function buySinglePageAndGetPageID(uint8[] memory _pagesAvailable, uint8[] memory _starsAvailable, bool _forceStar) private returns(uint8, uint8[] memory, uint8[] memory, uint8){ 
+        uint8[] memory randomIndex = new uint8[](1);
+        uint8[] memory pageID = new uint8[](1);
+        if(_forceStar) {
+            (uint8[] memory availableForcedPagesArray, uint elements) = getForcedStarArray(_starsAvailable);
+            elements > 0 ? randomIndex[0] = availableForcedPagesArray[getRandom(elements)] : randomIndex[0] = uint8(getRandom(_pagesAvailable.length));    
+        } else {
+            randomIndex[0] = uint8(getRandom(_pagesAvailable.length));           
+        }
+        pageID[0] = _pagesAvailable[randomIndex[0]];
+        _safeMint(msg.sender, pageID[0]);
+        expirationBlock[pageID[0]] = (block.number).add(blockDelay);
+        uint8 stars = _starsAvailable[randomIndex[0]];
+        return (uint8(pageID[0]), shiftArray8(_pagesAvailable, randomIndex), shiftArray8(_starsAvailable, randomIndex), stars);
     }
 
     function withdrawOwnerBalance(address payable _to) public onlyOwner returns(bool){
@@ -193,12 +276,10 @@ contract MetaborgStars is ERC721Upgradeable {
         return true;
     }
 
-    function getAddressMetadata(address _address) public view returns(uint, uint, uint, uint, uint, uint){
+    function getAddressMetadata(address _address) public view returns(uint8, uint8, uint8, uint, uint, uint){
         groupPriceStruct memory groupPrice = groupPriceMetaData[getUserGroup(_address)];
-        if(groupPrice.price1 == uint(0)){ // if not defined
-            groupPrice = groupPriceMetaData[0]; // default
-        }
-        return (groupPrice.pack1, groupPrice.pack2, groupPrice.pack3, groupPrice.price1, groupPrice.price2, groupPrice.price3);
+        if(groupPrice.price1 == uint(0)) groupPrice = groupPriceMetaData[0]; 
+        return (uint8(groupPrice.pack1), uint8(groupPrice.pack2), uint8(groupPrice.pack3), groupPrice.price1, groupPrice.price2, groupPrice.price3);
     }
 
     function burnAndReceivePhysicalEdition(uint _tokenID, string memory _email) public returns(bool){
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,47 +1,72 @@
-require('dotenv').config();
-require("@nomiclabs/hardhat-ethers");
-require("@nomiclabs/hardhat-etherscan");
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
 
-const { API_URL, PRIVATE_KEY } = process.env;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+import { HardhatUserConfig } from "hardhat/types";
 
-module.exports = {
-    solidity: {
-        // compilers: [{ version: "0.4.18", settings: {} }],
-        // compilers: [{ version: "0.5.0", settings: {} }],
-        // compilers: [{ version: "0.7.6", settings: {} }],
-        compilers: [
-            {
-            version: "0.8.0"
-            },
-            {
-            version: "0.8.1"
-            }, 
-            {
-            version: "0.8.3", 
-            settings: {} 
-            }
-        ],
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-etherscan";
+import "hardhat-abi-exporter";
+import "@nomiclabs/hardhat-web3";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-chai-matchers";
+
+// TODO: reenable solidity-coverage when it works
+// import "solidity-coverage";
+//const mocha = require("./mocha-config");
+
+const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const MNEMONIC = process.env.MNEMONIC || ""; 
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  mocha: {
+    timeout: 100000000000
+  },
+  solidity: {
+    // compilers: [{ version: "0.4.18", settings: {} }],
+    // compilers: [{ version: "0.5.0", settings: {} }],
+    // compilers: [{ version: "0.7.6", settings: {} }],
+    compilers: [{ version: "0.8.0"}, {version: "0.8.1"}, {version: "0.8.3", settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10,
+      },
+    }, }],
+  },
+  abiExporter: {
+    path: './abi',
+    clear: true,
+    flat: true,
+    // only: [':ERC721$'],
+    spacing: 2
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: MNEMONIC,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 10
+      }
     },
-    defaultNetwork: "rinkeby",
-    networks: {
-        hardhat: {},
-        rinkeby: {
-            url: API_URL,
-            accounts: [`0x${PRIVATE_KEY}`]
-        },
-        goerli: {
-          url: API_URL,
-          accounts: [`0x${PRIVATE_KEY}`]
-        },
-        mainnet: {
-            url: API_URL,
-            accounts: [`0x${PRIVATE_KEY}`]
-        }
+    localhost: {
+      accounts: {
+        mnemonic: MNEMONIC,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 10
+      }
     },
-    etherscan: {
-        // Your API key for Etherscan
-        // Obtain one at https://etherscan.io/
-        apiKey: ETHERSCAN_API_KEY
-    }
+    coverage: {
+      url: "http://127.0.0.1:8555", // Coverage launches its own ganache-cli client
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
+
+export default config;
